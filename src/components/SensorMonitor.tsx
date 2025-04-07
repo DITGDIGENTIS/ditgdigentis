e5fbb9c394689d349cd133e25d59a129954fbf1a
proectic <proectic@gmail.com>
2025-04-08 02:42:57 +0300

    zona temperature

--- src/components/SensorMonitor.tsx
+++ src/components/SensorMonitor.tsx
@@ -1,26 +1,29 @@
-'use client';
+"use client";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTint, faThermometerHalf } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTint, faThermometerHalf } from "@fortawesome/free-solid-svg-icons";
 
 export function SensorMonitor() {
   return (
     <div className="container sensor-container">
-      <h2 className="text-center">Середні показники:</h2>
       <div className="row">
         <div className="col-6 col-md-6">
-          <div className="average-humidity-block">
-            <div className="average-humidity-label">
-              <FontAwesomeIcon icon={faTint} />{' '}
-              <span id="averageHumidity" className="average-humidity-data">-- %</span>
+          <div className="top-average-humidity-block">
+            <div className="top-average-humidity-label">
+              <FontAwesomeIcon icon={faTint} />{" "}
+              <span id="averageHumidity" className="top-average-humidity-data">
+                -- %
+              </span>
             </div>
           </div>
         </div>
         <div className="col-6 col-md-6">
-          <div className="average-temp-block">
-            <div className="average-temp-label">
-              <FontAwesomeIcon icon={faThermometerHalf} />{' '}
-              <span id="averageTemperature" className="average-temp-data">-- °C</span>
+          <div className="top-average-temp-block">
+            <div className="top-average-temp-label">
+              <FontAwesomeIcon icon={faThermometerHalf} />{" "}
+              <span id="averageTemperature" className="top-average-temp-data">
+                -- °C
+              </span>
             </div>
           </div>
         </div>
@@ -29,25 +32,80 @@ export function SensorMonitor() {
       <h2 className="text-center mt-4 mb-4">Мониторинг сенсоров:</h2>
 
       <div className="row">
-        {[1, 2, 3, 4].map((i) => (
-          <div key={i} className="col-md-3 col-sm-6">
-            <div className="data-sensor">
-              <div className="data-sensor-label">Sensor:{i} (Температура)</div>
-              <div id={`sensor${i}`} className="data-sensor-data">-- °C</div>
+        <div className="col-3 col-md-3">
+        <div className="average-temp-block">
+            <div className="average-temp-label"> 
+              1:S1 <FontAwesomeIcon icon={faThermometerHalf} /> {""}
+              <span id="sensor1" className="average-temp-data">
+                -- °C
+              </span>
             </div>
           </div>
-        ))}
+        </div>
+        <div className="col-3 col-md-3">
+          <div className="average-temp-block">
+            <div className="average-temp-label">
+              1:S2 <FontAwesomeIcon icon={faThermometerHalf} /> {" "}
+              <span id="sensor2" className="average-temp-data">
+                -- °C
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="col-3 col-md-3">
+        <div className="average-temp-block">
+            <div className="average-temp-label">
+              2:S1 <FontAwesomeIcon icon={faThermometerHalf} /> {" "}
+              <span id="sensor3" className="average-temp-data">
+                -- °C
+              </span>
+            </div>
+          </div>
+        </div>  
+        <div className="col-3 col-md-3">
+        <div className="average-temp-block">
+            <div className="average-temp-label">
+              3:S1 <FontAwesomeIcon icon={faThermometerHalf} /> {" "}
+              <span id="sensor4" className="average-temp-data">
+                -- °C
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="row">
-        {[5, 6, 7].map((i) => (
-          <div key={i} className="col-md-4 col-sm-6">
-            <div className="data-sensor">
-              <div className="data-sensor-label">Sensor:{i} (Влажность)</div>
-              <div id={`sensor${i}`} className="data-sensor-data">-- %</div>
+      <div className="col-4 col-md-4">
+        <div className="average-humidity-block">
+            <div className="average-humidity-label">
+              <FontAwesomeIcon icon={faTint} />{" "}
+              <span id="sensor5" className="average-humidity-data">
+                -- %
+              </span>
             </div>
           </div>
-        ))}
+        </div>
+        <div className="col-4 col-md-4">
+          <div className="average-humidity-block">
+            <div className="average-humidity-label">
+              <FontAwesomeIcon icon={faTint} />{" "}
+              <span id="sensor6" className="average-humidity-data">
+                -- %
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-4 col-md-4">
+        <div className="average-humidity-block">
+            <div className="average-humidity-label">
+              <FontAwesomeIcon icon={faTint} />{" "}
+              <span id="sensor7" className="average-humidity-data">
+                -- %
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
