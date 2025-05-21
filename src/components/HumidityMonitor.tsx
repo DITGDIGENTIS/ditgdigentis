e8f317efe97dcb48b549e977fb80442bd9c7d1ca
proectic <proectic@gmail.com>
2025-05-21 20:09:22 +0300

    type button test

--- src/components/HumidityMonitor.tsx
+++ src/components/HumidityMonitor.tsx
@@ -363,7 +363,8 @@ export function HumidityMonitor() {
       
       <div className="row justify-content-center mb-4">
         <div className="col-12 col-md-8 text-center">
-          <button 
+          <button
+            type="button"
             onClick={isConnected ? disconnectFromSensor : connectToSensor}
             className={`btn ${isConnected ? 'btn-danger' : 'btn-primary'} mb-3`}
           >
